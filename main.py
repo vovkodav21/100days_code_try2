a3d269d33858ee4abfc50a4e2460523578353ab2
Vovkodav21 <vishtalyurii@gmail.com>
2023-07-11 20:31:56 +0200

    **Rules:** a bunch of kids sitting in a circle and going clockwise. A first kid says *"one"*, the second kid says *"two"*, but then, when a kid is facing a number which is fully divisible by "3", instead of saying a number he is going to say **Fizz**. And if the number is divisible by 5, they should say **Buzz**. And if a number is both divisible by 3 and 5, for example 15, than they should say **Fizz Buzz**.

---
+++ main.py
@@ -0,0 +1,12 @@
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        #divisible by 3 and by 5
+     print("FizzBuzz")
+    elif number % 3 == 0:
+    #divisible by 3
+      print("Fizz")
+    elif number % 5 == 0:
+    #divisible by 5
+      print("Buzz")
+    else:
+        print(number)
