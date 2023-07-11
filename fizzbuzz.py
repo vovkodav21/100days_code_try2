809db5bba7cab85d1efd7c7a9984fd0fbd3ea409
vovkodav21 <106028387+vovkodav21@users.noreply.github.com>
2023-07-11 20:37:33 +0200

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
-        #divisible by 3 and by 5
+    #divisible by 3 and by 5
      print("FizzBuzz")
     elif number % 3 == 0:
     #divisible by 3
